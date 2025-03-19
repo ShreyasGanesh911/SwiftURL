6e965c2512c2f26719f04273db2684c908b00cf3
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-03-19 22:36:04 +0530

    added URL model

---
+++ src/models/url.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose"
+const UrlSchema = new Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:[true,"User ID is required"]
+    },
+    category:{
+        type:String,
+        required:true,
+        enum:[ "Social", "Business", "News", "Shopping", "Education", 
+            "Entertainment", "Personal", "Work", "Tech", "Other"],
+        default:"Other"
+    },
+    shortUrl:{
+        type:String,
+        required:true,
+        unique:[true,"Short URL already exists"]
+    },
+    longUrl:{
+        type:String,
+        required:true,
+    },
+    clickCount:{
+        type:Number,
+        default:0
+    },
+    
+    firstClick:{
+        type:Date,
+    },
+    lastClick:{
+        type:Date,
+    },
+    deviceTypes:{
+        type: Map,
+        of: Number,
+    },
+    created_at:{
+        type:Date,
+        default:Date.now
+    },
+
+})
+const Url = mongoose.model("Url", UrlSchema)
+export default Url;
