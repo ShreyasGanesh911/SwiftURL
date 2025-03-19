0f9621487c1831559494c1417a630992610bd026
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-03-19 22:35:00 +0530

    added mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import fastify from "./app";
-const port = 3000
-fastify.listen({port}, (err,_) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`server listening on ${port}`);
-});
+import connection from "./db/mongo";
+import "dotenv/config";
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
+connection().then(()=>{
+  console.log("Connected to MongoDB")
+  fastify.listen({port:port}, (err,_) => {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`server listening on ${port}`);
+  });
+}).catch(e=>{
+  console.log("-------------Error connecting to MongoDB--------------")
+  console.log(e)
+  process.exit(1)
+})
