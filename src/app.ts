315e2e3e689647e917edbbd13f4d8127a670b173
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-03-19 22:34:49 +0530

    added functions

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import Fastify from "fastify";
-
 import APIFunction from "./routes/api.route";
-import RateLimiter from "./middleware/rateLimiter";
+import UserFunctions from "./routes/user.route";
+import URLFunction from "./routes/url.route";
 const fastify = Fastify();
-fastify.register(APIFunction,{
-    prefix:'/api/v1/data',
-    
-});
+fastify.register(APIFunction,{prefix:'/api/v1/data'});
+fastify.register(UserFunctions,{prefix:'/api/v1/user'});
+fastify.register(URLFunction,{prefix:'/api/v1/url'});
 
-export default fastify;
+export default fastify; 
