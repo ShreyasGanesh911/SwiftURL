6a5245acc84245a3d128c37e00921bda70f476c5
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-03-20 19:57:00 +0530

    Added fastify cookie

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import Fastify from "fastify";
 import APIFunction from "./routes/api.route";
 import UserFunctions from "./routes/user.route";
 import URLFunction from "./routes/url.route";
+import cookie from '@fastify/cookie'
+
 const fastify = Fastify();
-fastify.register(APIFunction,{prefix:'/api/v1/data'});
+fastify.register(cookie)
+fastify.register(APIFunction,{prefix:'/'});
 fastify.register(UserFunctions,{prefix:'/api/v1/user'});
 fastify.register(URLFunction,{prefix:'/api/v1/url'});
 
