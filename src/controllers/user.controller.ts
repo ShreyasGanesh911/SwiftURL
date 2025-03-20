d16e31922103228d60d70bab32081f2725809a0f
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-03-20 19:58:52 +0530

    added JWT authentication

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,10 @@
-import { FastifyInstance, FastifyReply } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import User from "../models/user.model";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config"
+const JWT_KEY = process.env.JWT_KEY || "qwerty"
+const SALT_ROUNDS = Number(process.env.SALT_ROUNDS) || 10
 interface UserInstance extends FastifyInstance{
     body:{
         name:string,
@@ -13,7 +17,7 @@ export const userRegister = async(req:UserInstance,reply:FastifyReply)=>{
         const exist = await User.findOne({email});
         if(exist)
             return reply.status(409).send({success:false,message:"User already exists"})
-        const hashedPassword = bcrypt.hashSync(password, 10);
+        const hashedPassword = bcrypt.hashSync(password, SALT_ROUNDS);
         const data = await User.create({name,email,password:hashedPassword});
         const {password:_,...user} = data.toObject();
         reply.status(201).send({
@@ -34,8 +38,14 @@ export const userLogin = async(req:UserInstance,reply:FastifyReply)=>{
     const checkPassword = bcrypt.compareSync(password,user.password);
     if(!checkPassword)
         return reply.status(401).send({success:false,message:"Incorrect Password"})
-    reply.status(200).send({
+    const {password:_,...data} = user.toObject()
+    const cookieAuth = await jwt.sign(data,JWT_KEY,{expiresIn:"1d"})
+    reply.setCookie('auth',cookieAuth,{path:"/",httpOnly:true,secure:true,maxAge:1* 24 * 60 * 60 * 1000}).status(200).send({
         success:true,
         message:"User logged in successfully"
     })
+}
+
+export const userLogout = async(req:FastifyRequest,reply:FastifyReply)=>{
+    reply.clearCookie('auth',{path:"/"}).status(200).send({success:true,message:"User logged out"})
 }
